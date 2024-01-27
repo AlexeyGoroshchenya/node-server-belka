d98ad1f9af32d87b164993e4741ac4ec20d39b9a
Алексей Горощеня <lastpaladin13@gmail.com>
2024-01-27 17:25:02 +0300

    before hosting

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const {Sequelize} = require('sequelize')
 
 console.log(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_HOST, process.env.DB_NAME);
 
-module.exports = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
+module.exports = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`,
     // `${process.env.DB_NAME}`,
     // `${process.env.DB_USER}`,
     // `${process.env.DB_PASSWORD}`,
