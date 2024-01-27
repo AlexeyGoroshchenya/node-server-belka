6099fdc0969e38fc819a138f0b2f7cb9242bb85d
Алексей Горощеня <lastpaladin13@gmail.com>
2024-01-27 17:54:11 +0300

    before hosting

--- db.js
+++ db.js
@@ -5,15 +5,14 @@ const {Sequelize} = require('sequelize')
 
 module.exports = new Sequelize(
     
-    
-
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    null,
     {
         dialect: 'postgres',
-        database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        username:process.env.DB_USER,
-        password:process.env.DB_PASSWORD,  
+
     }
 
 )
