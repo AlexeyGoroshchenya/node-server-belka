e8677449a07be90a666903eced14f9b997115d48
Алексей Горощеня <lastpaladin13@gmail.com>
2024-01-27 17:10:58 +0300

    before hosting

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
 const {Sequelize} = require('sequelize')
 
 
+console.log(process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_HOST, process.env.DB_NAME);
 
-
-module.exports = new Sequelize("postgres://postgres:postgres@localhost/belka",
+module.exports = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
     // `${process.env.DB_NAME}`,
     // `${process.env.DB_USER}`,
     // `${process.env.DB_PASSWORD}`,
-    {
-        dialect: 'postgres',
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-    }
+    // {
+    //     dialect: 'postgres',
+    //     host: process.env.DB_HOST,
+    //     port: process.env.DB_PORT,
+    // }
 
 )
